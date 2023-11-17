199371410177ab8d11b116d8138ca5fddf9caf75
Bolivegano <bolivegano@proton.me>
2023-11-17 14:45:36 -0600

    project finished

---
+++ simple_blog/blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_added']
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_added']
+
+    def __str__(self):
+        return self.name
+
